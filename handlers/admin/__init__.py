a521a01805f5595750a59322a6f2cedb85852299
egoza17499 <meha99.74@yandex.ru>
2026-02-20 16:42:12 +0300

    Update __init__.py

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -1,15 +1,12 @@
 from aiogram import Router
-from . import start, profile, search, list, manage, stats, test  # Добавили test
+from . import list, stats, airports, manage, test
 
 router = Router()
 
-# Регистрируем роутеры
-router.include_router(start.router)
-router.include_router(profile.router)
-router.include_router(search.router)
 router.include_router(list.router)
-router.include_router(manage.router)
 router.include_router(stats.router)
-router.include_router(test.router)  # Добавили тест
+router.include_router(airports.router)
+router.include_router(manage.router)
+router.include_router(test.router)
 
 __all__ = ["router"]
