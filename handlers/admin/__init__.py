1daf06dfb7c7b208beb4f871c45e54b139463b08
egoza17499 <meha99.74@yandex.ru>
2026-02-20 14:49:11 +0300

    Create __init__.py

---
+++ handlers/admin/__init__.py
@@ -0,0 +1,11 @@
+from aiogram import Router
+from .list import router as list_router
+from .stats import router as stats_router
+from .airports import router as airports_router
+from .manage import router as manage_router
+
+router = Router()
+router.include_router(list_router)
+router.include_router(stats_router)
+router.include_router(airports_router)
+router.include_router(manage_router)
