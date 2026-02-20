d7b6a0f33d2eee31345e5f95ee067f3a25a93d16
egoza17499 <meha99.74@yandex.ru>
2026-02-20 15:18:13 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,10 +4,7 @@ from .profile import router as profile_router
 from .search import router as search_router
 from .callbacks import router as callbacks_router
 from .text_handler import router as text_router
-from .admin.list import router as admin_list_router
-from .admin.stats import router as admin_stats_router
-from .admin.airports import router as admin_airports_router
-from .admin.manage import router as admin_manage_router
+from .admin import router as admin_router
 
 # Создаем главный роутер
 router = Router()
@@ -17,8 +14,5 @@ router.include_router(start_router)
 router.include_router(profile_router)
 router.include_router(search_router)
 router.include_router(callbacks_router)
-router.include_router(admin_list_router)
-router.include_router(admin_stats_router)
-router.include_router(admin_airports_router)
-router.include_router(admin_manage_router)
+router.include_router(admin_router)  # ✅ Подключаем весь admin одним роутером
 router.include_router(text_router)  # Последний!
